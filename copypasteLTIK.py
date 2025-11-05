8e863b00419794dc05ecaa4cab869d134267032c
Zpthin <najahahpatin.07@student.upi.edu>
2025-11-05 18:56:10 +0700

    Create copypasteLTIK.py

---
+++ copypasteLTIK.py
@@ -0,0 +1,19 @@
+def login():
+    pw = "Latihan"
+    kesempatan = 3
+    
+    for i in range (kesempatan):
+        print("Username: Daspro2023")
+        password = input("Masukkan password: ")
+        
+        if password == pw:
+            print("Login berhasil!")
+            break
+        else:
+            sisa = kesempatan - (i + 1)
+            if sisa > 0:
+                print(f"Password salah! Kesempatan tersisa {sisa}")
+            else:
+                print("Kesempatan habis! Anda keluar dari login")
+                
+login()
